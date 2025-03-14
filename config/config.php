1dace8a24b9a0cd90aff0ee9b10f0784a2f50cad
ellas2002 <104872775+ellas2002@users.noreply.github.com>
2025-03-14 23:29:50 +0000

    api-key now working

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
     /* Define API key as constant. Include this file in src/functions.php */
 
-    const  API_KEY = " live_X3nL95MER6PkRGDjLKeLy7annhBzLx1TTnOoh8Mbs85QPoQqMP0sdi7ft5MiAVVv ";
+    const  API_KEY = "live_XkcPNIPT3K7zPQcnKOFffxFD9XZiOcfsBOUy4OyqIQHCSlaMKHrCoHDqH2z2z6CW ";
     
-    $apiUrl = " https://api.thecatapi.com/v1/images/search?limit=10&api_key=x-api-key" ;
+    $url = "https://api.thecatapi.com/v1/images/search?limit=100&api_key=" . API_KEY ;
     
     /*use file_get_contents for the query*/
-    $response = file_get_contents("$apiUrl");
+    $response = file_get_contents("$url");
 
     if($response == false){
         die("error in getting api");
@@ -15,6 +15,5 @@
         /*json_decode to convert the response to a PHP object.*/
         $obj = json_decode($response);
     }
-
 
 ?>
